9885462f544eeb8a0ff547fee89d079f88b28b87
GustavoDiego <gustavodiego298@gmail.com>
2024-09-15 18:19:19 -0300

    refactor(css): excluir os arquivos inúteis  de css

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 
-
-
 const app = express()
 app.use(json())
 app.use(express.static(path.join(__dirname, 'src/view/public')))
